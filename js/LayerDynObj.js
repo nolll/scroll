d42e6c353a417949a805accdc67d654c7327ab12
Henrik <henriks@gmail.com>
2022-06-16 14:15:26 +0200

    Removed browser detection, comments and a few document.all references

--- js/LayerDynObj.js
+++ js/LayerDynObj.js
@@ -24,96 +24,53 @@ function LayerDynObj(id, left, top, width, height, visibility, zIndex, clip, bgC
 	this.bgColor = (bgColor != null) ? bgColor : null;
 	this.background = (background != null) ? background : null;
    	    
-	if (nav.ns4) {
-		this.css = this.elm = this.event = document.layers[this.id] = new Layer(width);	
-		this.doc = this.css.document;
-		
-		this.css.zIndex 	= this.zIndex;
-		this.css.visibility = (this.visible) ? "show" : "hide";
-		
-		this.css.left 	= this.x;
-		this.css.top 	= this.y;
-		
-		if (this.bgColor != null) this.css.bgColor = this.bgColor;
-		if (this.background != null) this.css.background.src = this.background;
-        
-		this.css.clip.top 		= this.clipRect.top;
-		this.css.clip.right 	= this.clipRect.right;
-		this.css.clip.bottom 	= this.clipRect.bottom;	
-		this.css.clip.left 		= this.clipRect.left;
-		
-		this.innerHtml = "";
-		if (htmlContent != null){
-			this.css.document.open();
-			this.css.document.write(htmlContent);
-			this.css.document.close();
-			this.innerHtml = htmlContent;
-		}
-		
-		this.contentW = this.css.document.width;
- 		this.contentH = this.css.document.height;	
-	}  
-	else if (nav.ie) {
-		var str = '' + 
-		'<DIV id="' + this.id + '" style="position:absolute; left:' + this.x + '; top:' + this.y +
-        '; width:' + this.w + '; height:' + this.h + 
-        '; visibility:' + (this.visible == true  ? "visible" : "hidden") + 
-		(this.bgColor == null ? "" : ("; background-color:" + this.bgColor )) +
-		(this.background == null ? "" : ("; background-image:url('" + this.background + "')")) +
-		'; clip:rect('+this.clipRect.top+'px '+this.clipRect.right+'px '+this.clipRect.bottom+'px '+this.clipRect.left+'px);';
-        
-        str += ' z-index:' + this.zIndex + ';';
-        str += ' overflow:hidden;">\n';
-		if (htmlContent != null)
-			str += htmlContent;
-		str += '\n</DIV>\n';
-		
-      	document.body.insertAdjacentHTML("BeforeEnd", str);
-			 
-		this.elm = this.event  = document.all[id];
-		this.css = document.all[id].style;
-		this.doc = document;
-		
-		this.w = (nav.ie4)? this.css.pixelWidth : this.elm.offsetWidth;
-		this.h = (nav.ie4)? this.css.pixelHeight : this.elm.offsetHeight;
+	var str = '' + 
+	'<DIV id="' + this.id + '" style="position:absolute; left:' + this.x + '; top:' + this.y +
+	'; width:' + this.w + '; height:' + this.h + 
+	'; visibility:' + (this.visible == true  ? "visible" : "hidden") + 
+	(this.bgColor == null ? "" : ("; background-color:" + this.bgColor )) +
+	(this.background == null ? "" : ("; background-image:url('" + this.background + "')")) +
+	'; clip:rect('+this.clipRect.top+'px '+this.clipRect.right+'px '+this.clipRect.bottom+'px '+this.clipRect.left+'px);';
 	
-		this.contentW = this.elm.scrollWidth;
- 		this.contentH = this.elm.scrollHeight;		
-	}
+	str += ' z-index:' + this.zIndex + ';';
+	str += ' overflow:hidden;">\n';
+	if (htmlContent != null)
+		str += htmlContent;
+	str += '\n</DIV>\n';
 	
+	document.body.insertAdjacentHTML("BeforeEnd", str);
+			
+	this.elm = this.event = document.getElementById(id);
+	this.css = this.elm.style;
+	this.doc = document;
+	
+	this.w = this.elm.offsetWidth;
+	this.h = this.elm.offsetHeight;
+
+	this.contentW = this.elm.scrollWidth;
+	this.contentH = this.elm.scrollHeight;		
+
 	this.elm.parent = this;
 }
 
 function LayerDynObj_show() {
-	this.css.visibility = (nav.ns4)? "show" : "visible";
+	this.css.visibility = "visible";
 }
 
 function LayerDynObj_hide() {
-	this.css.visibility = (nav.ns4)? "hide" : "hidden";
+	this.css.visibility = "hidden";
 }
 
 function LayerDynObj_writeHtml(html) {
 	this.innerHtml = html;
-	if (nav.ns4) {	
-		this.doc.open();
-		this.doc.write(html);
-		this.doc.close();
-
-		this.contentW = this.css.document.width;  
- 		this.contentH = this.css.document.height; 
-	}
-	else if (nav.ie) {
-		this.event.innerHTML = html;
-		this.contentW = this.elm.scrollWidth;
-		this.contentH = this.elm.scrollHeight; 
-	}
+	this.event.innerHTML = html;
+	this.contentW = this.elm.scrollWidth;
+	this.contentH = this.elm.scrollHeight; 
 }
 
 function LayerDynObj_deleteObject(){
     this.hide();
 	this.writeHtml("");
-	if (is.ns4)
-		delete this.css;
 }
 
 function LayerDynObj_moveTo(x,y,changeInitVals) {	
@@ -125,27 +82,15 @@ function LayerDynObj_moveTo(x,y,changeInitVals) {
 		this.y = y;
 	}
 		
-	if (nav.ns4) { 
-		this.css.left = this.x;
-		this.css.top = this.y;
-	}	
-	else if (nav.ie) { 
-		this.css.pixelLeft = this.x;
-		this.css.pixelTop = this.y;
-	} 
+	this.css.pixelLeft = this.x;
+	this.css.pixelTop = this.y;
 }
 
 function LayerDynObj_moveBy(x,y) {	
 	this.x += (x == null) ? 0 : x;
 	this.y += (y == null) ? 0 : y;
-	if (nav.ns4) { 
-		this.css.left = this.x;
-		this.css.top = this.y;
-	}	
-	else if (nav.ie) { 
-		this.css.pixelLeft = this.x;
-		this.css.pixelTop = this.y;
-	}
+	this.css.pixelLeft = this.x;
+	this.css.pixelTop = this.y;
 }
 
 function LayerDynObj_changeClip(top, right, bottom, left) {
@@ -153,14 +98,7 @@ function LayerDynObj_changeClip(top, right, bottom, left) {
 	this.clipRect.right  = parseInt(right);
 	this.clipRect.bottom = parseInt(bottom);
 	this.clipRect.left   = parseInt(left);	
-	if (nav.ns4) { 
-		this.css.clip.top 		= this.clipRect.top; 
-		this.css.clip.right 	= this.clipRect.right; 
-		this.css.clip.bottom 	= this.clipRect.bottom; 
-		this.css.clip.left 		= this.clipRect.left;
-	} else { 
-		this.css.clip = "rect("+ this.clipRect.top +"px "+ this.clipRect.right +"px "+ this.clipRect.bottom +"px "+ this.clipRect.left +"px)";
-	}
+	this.css.clip = "rect("+ this.clipRect.top +"px "+ this.clipRect.right +"px "+ this.clipRect.bottom +"px "+ this.clipRect.left +"px)";
 }
 
 function LayerDynObj_resizeTo(width, height, saveTmpVals, changeClip) {
@@ -170,14 +108,9 @@ function LayerDynObj_resizeTo(width, height, saveTmpVals, changeClip) {
 	}
 	this.w = width;
 	this.h = height;
-			
-	if (nav.ns4) {  
-		this.css.resizeTo(width, height);
-	} else { 
-		this.css.pixelWidth = width; 
-		this.css.pixelHeight = height;
-		if (changeClip != false) this.changeClip(0,width,height,0);
-	} 
+	this.css.width = width; 
+	this.css.height = height;
+	if (changeClip != false) this.changeClip(0,width,height,0);
 }
 
 function LayerDynObj_resizeBy(dwidth, dheight, saveTmpVals, changeClip) {
@@ -191,11 +124,7 @@ function LayerDynObj_changeZIndex(z) {
 
 function LayerDynObj_setBgColor(color) {	
 	this.css.bgcolor = color;
-	if (nav.ns4) { 
-		this.css.bgColor = this.css.bgcolor;
-	} else if (nav.ie || nav.ns6) { 
-		this.css.backgroundColor = this.css.bgcolor;
-	} 
+	this.css.backgroundColor = this.css.bgcolor;
 }
 
 LayerDynObj.prototype.moveTo 		= LayerDynObj_moveTo;
--- js/RectObj.js
+++ js/RectObj.js
@@ -1,15 +1,3 @@
-/*
-Description:
-	- Rectobject
-	
-Used by:
-	- LayerObject_dyn.js
-	- LayerObject_dyn_nestled.js	
-
-Dependencies:
-
-*/
-
 function Rect(top, right, bottom, left) {
 	this.top 	= this.initTop 	  = parseInt(top); 
 	this.right 	= this.initRight  = parseInt(right);
